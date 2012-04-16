b35752663dfa986a68bd95402fe74889dfb0b897
Morten Kristensen <msk@nullpointer.dk>
2012-04-16 09:20:54 +0200

    Implemented retrieving nodes/lists from paths using the syntax "name/name2/name3" to separate node names in the tree.

--- src/lib/ConfigTree.cpp
+++ src/lib/ConfigTree.cpp
@@ -8,3 +8,58 @@ ConfigTree::~ConfigTree() {
     delete tree;
   }
 }
+
+ConfigTreeNode *ConfigTree::getNode(const QString &path) {
+  QStringList elms = pathToList(path);
+  if (elms.size() == 0) {
+    return NULL;
+  }
+
+  // Walk the path and find the node list.
+  ConfigTreeNode *node = tree;
+  while (elms.size() > 0) {
+    QString part = elms.takeFirst();
+    node = traverse(node, part);
+    if (!node) {
+      return NULL;
+    }
+  }
+
+  return node;
+}
+
+bool ConfigTree::getNodes(const QString &path, ConfigTreeNodeList &nodes) {
+  ConfigTreeNode *node = getNode(path);
+  if (node) {
+    nodes = node->getNodes();
+    return true;
+  }
+  return false;
+}
+
+void ConfigTree::print(QDebug dbg) {
+  tree->print(0, dbg);
+}
+
+QStringList ConfigTree::pathToList(const QString &path) {
+  QStringList elms = path.split("/");
+  if (elms.size() == 0) {
+    return elms;
+  }
+  if (elms[0] == tree->getName()) {
+    elms.removeFirst();
+  }
+  return elms;
+}
+
+ConfigTreeNode *ConfigTree::traverse(ConfigTreeNode *node, const QString &name) {
+  if (!node) return NULL;
+
+  foreach (ConfigTreeNode *elm, node->getNodes()) {
+    if (elm->getName() == name) {
+      return elm;
+    }
+  }
+
+  return NULL;
+}
--- src/lib/ConfigTree.h
+++ src/lib/ConfigTree.h
@@ -1,34 +1,43 @@
 #ifndef CONFIG_TREE_H
 #define CONFIG_TREE_H
 
-/*
-  Should also have methods for accessing leafs smartly, like
-  "ServerConfig.Agents" to retrieve a list of leafs at that point in
-  the tree.
- */
-
+#include <QDebug>
 #include <QString>
+#include <QStringList>
 
-class ConfigTreeNode;
+#include "ConfigTreeNode.h"
 
 class ConfigTree {
 public:
   ConfigTree(const QString &name = QString("root"));
   virtual ~ConfigTree();
-  
+
+  /**
+   * Get the node for the given path.
+   */
+  ConfigTreeNode *getNode(const QString &path);  
+
+  /**
+   * Get node list for the given path. Returns false if nothing was
+   * found.
+   */
+  bool getNodes(const QString &path, ConfigTreeNodeList &nodes);
+
   // Add node at specified point in the tree and returns the node it
   // was added to.
   //ConfigTreeNode *addNode(const QString &path, ConfigTreeNode *node);
 
   // Add node to root node.
-  //ConfigTreeNode *addNode(ConfigTreeNode *node);
-
-  // Get node list from path.
-  //ConfigTreeNodeList getNode(const QString &path);
+  //ConfigTreeNode *addNode(ConfigTreeNode *node);  
   
   ConfigTreeNode *getRoot() const { return tree; }
+
+  void print(QDebug dbg = QDebug(QtDebugMsg));
     
 private:
+  QStringList pathToList(const QString &path);
+  ConfigTreeNode *traverse(ConfigTreeNode *node, const QString &name);
+  
   ConfigTreeNode *tree;
 };
 
