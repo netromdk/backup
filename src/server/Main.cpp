9f0dc14444f182ac7e038f03938581c3bb7e1274
Morten Kristensen <msk@nullpointer.dk>
2012-04-21 01:18:00 +0200

    Registered signals SIGABRT, SIGTERM, SIGINT and SIGKILL.

--- src/server/Main.cpp
+++ src/server/Main.cpp
@@ -1,14 +1,46 @@
+#include <QList>
 #include <QDebug>
 #include <QCoreApplication>
+
+#include <signal.h>
 
 #include "Server.h"
 #include "Version.h"
 
+Server *server = NULL;
+
+void signalHandler(int signal) {
+  static QList<int> sup = QList<int>() << SIGABRT << SIGTERM << SIGINT << SIGKILL;
+  if (!sup.contains(signal)) {
+    qWarning() << "Caught unsupported signal:" << signal;
+    return;
+  }
+
+  qDebug() << "Caught signal. Terminating..";
+  
+  if (server) {
+    delete server;
+    server = NULL;
+  }
+    
+  QCoreApplication::exit(0);
+}
+
 int main(int argc, char **argv) {
   QCoreApplication app(argc, argv);
 
+  // Register signals for cleanup.
+  signal(SIGABRT, &signalHandler);
+	signal(SIGTERM, &signalHandler);
+	signal(SIGINT, &signalHandler);
+
+#ifndef WIN
+  // SIGKILL is not available on Windows.
+	signal(SIGKILL, &signalHandler);
+#endif  
+
   qDebug() << "Starting backup server -" << qPrintable(versionString());
-  Server server;
+  server = new Server;
   
   return app.exec();
 }
