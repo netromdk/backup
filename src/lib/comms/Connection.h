1452f81ab297f60cf3dce2465dba81a1550b1f9c
Morten Kristensen <msk@nullpointer.dk>
2012-04-21 14:53:01 +0200

    Added descriptive comments for SslServer and Connection.

--- src/lib/comms/Connection.h
+++ src/lib/comms/Connection.h
@@ -3,11 +3,21 @@
 
 #include <QSslSocket>
 
+/**
+ * Provides a socket using SSL.
+ */
 class Connection : public QSslSocket {
   Q_OBJECT
   
 public:
+  /**
+   * Creates a socket for client mode.
+   */
   Connection();
+
+  /**
+   * Creates a socket for server mode.
+   */
   Connection(int socketDescriptor, const QString &cert, const QString &key);
 
 private slots:
--- src/lib/comms/SslServer.h
+++ src/lib/comms/SslServer.h
@@ -6,6 +6,12 @@
 
 class Connection;
 
+/**
+ * Provides a TCP server for secure communications using SSLv3/TLSv1.
+ *
+ * Only allows ciphers that use at least 128 bits and disallows all
+ * using MD5, RC4 or DES.
+ */
 class SslServer : public QTcpServer {
   Q_OBJECT
 
