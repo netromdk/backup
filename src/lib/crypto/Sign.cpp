5f9c83fa34076bc43ff317e6ee8031bdc0aef836
Morten Kristensen <msk@nullpointer.dk>
2012-04-22 11:44:04 +0200

    Sign/verify uses specified digest algorithm.

--- src/lib/crypto/Sign.cpp
+++ src/lib/crypto/Sign.cpp
@@ -4,7 +4,8 @@
 #include "Sign.h"
 
 namespace crypto {
-  bool sign(const QByteArray &data, const QSslKey &key, QByteArray &sig) {
+  bool sign(const QByteArray &data, const QSslKey &key, DigestAlgorithm alg,
+            QByteArray &sig) {
     const unsigned char *cdata = (const unsigned char*) data.constData();
     
     switch (key.algorithm()) {
@@ -13,7 +14,7 @@ namespace crypto {
       if (!rsa) return false;
       unsigned char *sigret = new unsigned char[RSA_size(rsa)];
       unsigned int siglen;
-      if (RSA_sign(NID_sha512, cdata, data.size(), sigret, &siglen, rsa)) {
+      if (RSA_sign(alg, cdata, data.size(), sigret, &siglen, rsa)) {
         sig = QByteArray((char*) sigret, siglen);        
         return true;
       }
@@ -25,7 +26,7 @@ namespace crypto {
       if (!dsa) return false;
       unsigned char *sigret = new unsigned char[DSA_size(dsa)];
       unsigned int siglen;      
-      if (DSA_sign(NID_sha512, cdata, data.size(), sigret, &siglen, dsa)) {
+      if (DSA_sign(alg, cdata, data.size(), sigret, &siglen, dsa)) {
         sig = QByteArray((char*) sigret, siglen);        
         return true;
       }
@@ -36,7 +37,8 @@ namespace crypto {
     return false;
   }
 
-  bool verify(const QByteArray &sig, const QByteArray &data, const QSslKey &key) {
+  bool verify(const QByteArray &sig, const QByteArray &data, const QSslKey &key,
+              DigestAlgorithm alg) {
     const unsigned char *cdata = (const unsigned char*) data.constData(),
       *csig = (const unsigned char*) sig.constData();
     
@@ -44,7 +46,7 @@ namespace crypto {
     case QSsl::Rsa: {
       RSA *rsa = getRsaKey(key);
       if (!rsa) return false;
-      if (RSA_verify(NID_sha512, cdata, data.size(), csig, sig.size(), rsa)) {
+      if (RSA_verify(alg, cdata, data.size(), csig, sig.size(), rsa)) {
         return true;
       }
       break;
@@ -53,7 +55,7 @@ namespace crypto {
     case QSsl::Dsa: {
       DSA *dsa = getDsaKey(key);
       if (!dsa) return false;
-      if (DSA_verify(NID_sha512, cdata, data.size(), csig, sig.size(), dsa)) {
+      if (DSA_verify(alg, cdata, data.size(), csig, sig.size(), dsa)) {
         return true;
       }      
       break;
--- src/lib/crypto/Sign.h
+++ src/lib/crypto/Sign.h
@@ -4,16 +4,22 @@
 #include <QSslKey>
 #include <QByteArray>
 
+#include "Digest.h"
+
 namespace crypto {
   /**
-   * Sign data using a key to produce a SHA-512 message digest.
+   * Sign data using a key to produce a message digest using the
+   * specified algorithm.
    */
-  bool sign(const QByteArray &data, const QSslKey &key, QByteArray &sig);
+  bool sign(const QByteArray &data, const QSslKey &key, DigestAlgorithm alg,
+            QByteArray &sig);
 
   /**
-   * Verify signature of data using key.
+   * Verify signature of data using key and specified digest algorithm
+   * used at the time of signing.
    */
-  bool verify(const QByteArray &sig, const QByteArray &data, const QSslKey &key);  
+  bool verify(const QByteArray &sig, const QByteArray &data, const QSslKey &key,
+              DigestAlgorithm alg);  
 }
 
 #endif // SIGN_H
