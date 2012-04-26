44bc4feac3fd2bfda032431f242b28c227343134
Morten Kristensen <msk@nullpointer.dk>
2012-04-26 19:25:59 +0200

    Console interprets term vars like %u, %yyyy, %hh, %mm, %ss etc.

--- src/lib/cmd/CommandConsole.cpp
+++ src/lib/cmd/CommandConsole.cpp
@@ -1,3 +1,6 @@
+#include <QDateTime>
+
+#include "util/Utility.h"
 #include "CommandConsole.h"
 #include "CommandExecuter.h"
 #include "CommandTreeNode.h"
@@ -44,5 +47,27 @@ void CommandConsole::setTermString(const QString &term) {
 }
 
 QString CommandConsole::resolveTerm() {
-  return termString;
+  QString term = termString.replace("%u", Utility::getUserName());
+
+  QDateTime now = QDateTime::currentDateTime(); 
+  term = term.replace("%yyyy", now.toString("yyyy")); 
+  term = term.replace("%yy", now.toString("yy"));
+  term = term.replace("%MMMM", now.toString("MMMM"));    
+  term = term.replace("%MMM", now.toString("MMM"));  
+  term = term.replace("%MM", now.toString("MM"));
+  term = term.replace("%M", now.toString("M"));
+  term = term.replace("%dddd", now.toString("dddd"));    
+  term = term.replace("%ddd", now.toString("ddd"));  
+  term = term.replace("%dd", now.toString("dd"));
+  term = term.replace("%d", now.toString("d"));     
+  term = term.replace("%hh", now.toString("hh"));
+  term = term.replace("%h", now.toString("h"));   
+  term = term.replace("%mm", now.toString("mm"));
+  term = term.replace("%m", now.toString("m"));   
+  term = term.replace("%ss", now.toString("ss"));
+  term = term.replace("%s", now.toString("s"));   
+  term = term.replace("%AP", now.toString("AP")); 
+  term = term.replace("%ap", now.toString("ap")); 
+
+  return term;
 }
--- src/lib/cmd/CommandConsole.h
+++ src/lib/cmd/CommandConsole.h
@@ -19,8 +19,35 @@ public:
   /**
    * Sets the terminal string which appears before every command
    * prompt.
+   *
+   * The supported substitutional variables are:
+   * <table border=0 cellpadding=5>
+   * <tr><td>u</td><td>Yields the user name of the current user.</td></tr>
+   * <tr><td>yyyy</td><td>Yields the current year with four digits (e.g. 2012).</td></tr>
+   * <tr><td>yy</td><td>Yields the current year with two digits (e.g. 12).</td></tr>
+   * <tr><td>MMMM</td><td>Yields the current month in the longest form (e.g. April).</td></tr>
+   * <tr><td>MMM</td><td>Yields the current month in long form (e.g. Apr).</td></tr>
+   * <tr><td>MM</td><td>Yields the current month as a number with leading zeroes (e.g. 04).</td></tr>
+   * <tr><td>M</td><td>Yields the current month as a number without leading zeroes (e.g. 4).</td></tr>
+   * <tr><td>dddd</td><td>Yields the current day in the longest form (e.g. Thursday).</td></tr>
+   * <tr><td>ddd</td><td>Yields the current day in long form (e.g. Thu).</td></tr>
+   * <tr><td>dd</td><td>Yields the current day as a number with leading zeroes (e.g. 03).</td></tr>
+   * <tr><td>d</td><td>Yields the current day as a number without leading zeroes (e.g. 3).</td></tr>
+   * <tr><td>hh</td><td>Yields the current hour as a number with leading zeroes (e.g. 09).</td></tr>
+   * <tr><td>h</td><td>Yields the current hour as a number without leading zeroes (e.g. 9).</td></tr>
+   * <tr><td>mm</td><td>Yields the current minute as a number with leading zeroes (e.g. 06).</td></tr> 
+   * <tr><td>m</td><td>Yields the current minute as a number without leading zeroes (e.g. 6).</td></tr>
+   * <tr><td>ss</td><td>Yields the current second as a number with leading zeroes (e.g. 01).</td></tr> 
+   * <tr><td>s</td><td>Yields the current second as a number without leading zeroes (e.g. 1).</td></tr>
+   * <tr><td>AP</td><td>Yields either "PM" or "AM".</td></tr>
+   * <tr><td>ap</td><td>Yields either "pm" or "am".</td></tr>   
+   * </table>
+   *
+   * Use with "%" before them, e.g. "%MMM". <b>Note that all values
+   * follow the system locale.</b>
    */
   void setTermString(const QString &term);
+  
   QString getTermString() const { return termString; }  
 
 private:
